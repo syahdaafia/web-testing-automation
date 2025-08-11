7bfcc1cfe132e51273f5ba2f804dbcbc3022e012
Syahda Afia <76998317+syahdaafia@users.noreply.github.com>
2025-08-11 14:25:36 +0700

    Add files via upload

---
+++ CYPRESS -CUCUMBER/cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+const browserify = require("@cypress/browserify-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprendTransformerToOptions,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    browserify(preprendTransformerToOptions(config, browserify.defaultOptions)),
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://rahulshettyacademy.com/loginpagePractise/",
+    setupNodeEvents,
+  },
+});
